4aa9992e66227bf4896988bbdc2ea3c364bce199
filipecosta90 <filipecosta.90@gmail.com>
2016-03-03 01:31:51 +0000

    restructured symbol analyser

--- freq_analyse_cpp.cpp
+++ freq_analyse_cpp.cpp
@@ -4,63 +4,84 @@
 #include <stdio.h>
 #include <wchar.h>
 #include <map>
+#include <set>
 #include <string.h>
+#include <bitset>         // std::bitset
+#include <vector>
 
+
+class Symbol {
+  public:
+    char32_t character;
+    long int absolut_freq;
+    float relative_freq;
+    std::vector < std::bitset<1>> codification;
+
+    bool operator<(const Symbol& other) const
+    {
+      return character < other.character;
+    }
+
+    void spotted(){
+      absolut_freq++;
+    };
+
+    struct compare {
+      bool operator()(const Symbol& a, const Symbol& b)const
+      {if (a.relative_freq != b.relative_freq) return a.relative_freq > b.relative_freq;
+        return a.character > b.character;
+      };
+    };
+
+    /* adds a new less significant bit*/
+    void add_less_sig_bit( std::bitset<1> s_bit ){
+      std::vector<std::bitset<1>>::iterator it;
+      it = codification.end();
+      codification.insert ( it , s_bit );
+    };
+
+};
+
+class SymbolTable{
+  public:
+    std::set <Symbol, Symbol::compare> symbols_table;
+    int distinct_symbols;
+    int total_count;
+
+    bool read_file( FILE* infile ){
+      for ( char32_t wc; wc != WEOF; wc = getwc(infile)){
+        Symbol current;
+        current.character = wc;
+        std::set<Symbol>::iterator got = symbols_table.find (current);
+        /* if the key doesn't exists*/
+        if ( got == symbols_table.end() ){
+          current.absolut_freq++;
+          symbols_table.insert(got,current);
+          distinct_symbols++;
+        }
+        /* if the key exists*/
+        else {
+          (*got)->spotted();
+        }
+        total_count++;
+      }
+      fclose(infile);
+      return EXIT_SUCCESS;
+    };
+};
 
 int main (int argc, char* argv[])
 {
-  if (argc != 3){
-    std::cerr << "invalid argument number" << std::endl;
-    return EXIT_FAILURE;
+  FILE* in;
+  in = fopen(argv[2], "r");
+  SymbolTable table;
+  bool result = table.read_file(in);
+  if ( result ){
+    return EXIT_SUCCESS;
   }
   else {
-    FILE *infile;
-    if (strcmp(argv[1],"rt")== 0){
-      infile=fopen(argv[2],"r,ccs=UTF-8");
-    }
-    else {
-      if (strcmp(argv[1],"rb")== 0){
-        infile=fopen(argv[2],"rb,ccs=UTF-8");
-      }
-      else {
-        std::cerr << "invalid file type" << std::endl;
-        return EXIT_FAILURE;
-      }
-    }
-    char32_t wc;
-
-    /* key: char32_t, value: absolute frequency */
-    std::map<char32_t, long int> frequency_map;
-
-    if (infile!=NULL){
-      while ((wc = getwc (infile))!=WEOF){
-        std::map<char32_t,long int>::const_iterator got = frequency_map.find (wc);
-        /* if the key exists*/
-        if ( got == frequency_map.end() ){
-          std::pair<char32_t,long int> new_pair (wc,1);
-          frequency_map.insert (new_pair); 
-        }
-        /* if the key doesn't exists*/
-        else {
-          frequency_map.at(wc)++;
-        }
-      }
-      fclose(infile);
-      /* print the frequency table*/
-      std::cout << "------------------" << std::endl;
-      std::cout << "Tabela Frequencias" << std::endl;
-      std::cout << "------------------" << std::endl;
-      long int total_caracters = 0;
-      long int total_distinct = frequency_map.size();
-      for ( auto it = frequency_map.begin(); it != frequency_map.end(); ++it ){
-        std::cout << it->first << "\t" << (it->second) <<std::endl;
-        total_caracters += it->second;
-      }
-      std::cout << "------------------" << std::endl;
-      std::cout <<  "total caracteres: " << total_caracters  << std::endl;
-      std::cout <<  "total caracteres distintos: " << total_distinct  << std::endl;
-    }
-    return EXIT_SUCCESS;
+    std::cerr << "invalid file type" << std::endl;
+    return EXIT_FAILURE;
   }
 }
 
